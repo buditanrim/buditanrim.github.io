e05c7c2869a7abc95b331130e20d551977f5c7a9
Budi Tanrim <buditanrim@gmail.com>
2018-09-08 11:51:29 -0400

    Add Grunt cssmin

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      build: {
+        src: 'js/global.js',
+        dest: 'js/global.min.js'
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin']);
+
+};
